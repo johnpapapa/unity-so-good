a263a20d2751632149f7f12cf7300e41db8c9af8
lagrimas <lagrimas@everylive.jp>
2023-08-30 16:03:19 +0900

    fix login page

--- src/templates/Users/add.php
+++ src/templates/Users/add.php
@@ -1,13 +1,28 @@
-<?php $this->assign('title', 'Users'); ?>
-<?php $this->assign('content-title', 'ユーザ新規登録'); ?>
+<?php $this->assign('title', 'register'); ?>
+<?php $this->assign('content-title', 'ユーザ-新規登録'); ?>
 <div class="users form">
 <?= $this->Form->create($user) ?>
     <fieldset>
-        <legend><?= __('Add User') ?></legend>
-        <?= $this->Form->control('username') ?>
-        <?= $this->Form->control('password') ?>
-        <?= $this->Form->control('display_name') ?>
+        <label for="username">ユーザーID</label>
+        <p style="font-size: 12px; color:gray">
+            ログイン時にユーザーIDとして入力する文字列<br>
+            半角英数字のみ
+        </p>
+        <input type="text" name="username" required="required" data-validity-message="This field cannot be left empty" oninvalid="this.setCustomValidity(''); if (!this.value) this.setCustomValidity(this.dataset.validityMessage)" oninput="this.setCustomValidity('')" id="username" aria-required="true" maxlength="255">
 
+        <label for="password">パスワード</label>
+        <p style="font-size: 12px; color:gray">
+            ログイン時にパスワードとして入力する文字列<br>
+            半角英数字のみ
+        </p>
+        <input type="password" name="password" id="password">
+
+        <label for="display-name">表示名</label>
+        <p style="font-size: 12px; color:gray">
+            アンケートやイベント作成時に他の人から見える名前<br>
+            半角全角可
+        </p>
+        <input type="text" name="display_name" required="required" data-validity-message="This field cannot be left empty" oninvalid="this.setCustomValidity(''); if (!this.value) this.setCustomValidity(this.dataset.validityMessage)" oninput="this.setCustomValidity('')" id="display-name" aria-required="true" maxlength="255">
    </fieldset>
 <?= $this->Form->button(__('Submit')); ?>
 <?= $this->Form->end() ?>
--- src/templates/Users/login.php
+++ src/templates/Users/login.php
@@ -1,8 +1,16 @@
+<?php $this->assign('title', 'login'); ?>
+<?php $this->assign('content-title', 'ログイン'); ?>
 
 <form method="post" name="login">
     <input type="hidden" name="_csrfToken" autocomplete="off" value="<?= $this->request->getAttribute('csrfToken') ?>">
-    <?= $this->Form->control('username') ?>
-    <?= $this->Form->control('password') ?>
+    <!-- <?= $this->Form->control('username') ?> -->
+    <label for="username">ユーザーID</label>
+    <input type="text" name="username" id="username">
+
+    <!-- <?= $this->Form->control('password') ?> -->
+    <label for="password">パスワード</label>
+    <input type="password" name="password" id="password">
+
     <button type="submit" name="login">login</button>
 </form>
 
