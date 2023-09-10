78ed29e1f920505e9df9b072fd745eb01e31d547
lagrimas <lagrimas@everylive.jp>
2023-09-10 17:05:01 +0900

    remove form

--- src/templates/Users/login.php
+++ src/templates/Users/login.php
@@ -20,39 +20,39 @@
         font-weight: bold;
     }
 </style>
+<p class="p-note mb20">
+    メンバー専用画面へアクセスするには<br>
+    ログインが必要です。
+</p>
+
 
 <div class="login-content disp-flex just-center">
     <div class="login form pure-g pure-form pure-form-stacked mb10">
         <?= $this->Form->create() ?>
             <div class="mb30">
-                <!-- <a href="https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=2000439541&redirect_uri=http://localhost:8001/users/lineLogin&state=12345abcde&scope=profile%20openid&nonce=09876xyz"> -->
                 <a href="<?= $url_linelogin ?>">
                     <button class="pure-button line-login-btn pure-u-1" type="button">
                         LINEでログイン
                     </button>
                 </a>
-
-                <p class="p-note">アカウント未作成の方がLINEログインを行うと<br>自動でアカウントが作成されます。</p>
-            </div>
-            <div class="mb10">
-                <label for="user_id">ユーザーID</label>
-                <input type="text" name="user_id" id="user_id">
+                <p class="p-note">
+                    アカウント未作成の方がLINEログインを行うと<br>
+                    自動でアカウントが作成されます。
+                </p>
             </div>
 
-            <div class="mb10">
-                <label for="password">パスワード</label>
-                <input type="password" name="password" id="password">
-            </div>
-        
-            <div class="mb10">
-                <button type="submit" name="login" class="pure-button pure-button-primary">ログイン</button>
+            <div class="mb30">
+                <a href="<?= $url_linelogin ?>">
+                    <button class="pure-button admin-login-btn pure-u-1" type="button">
+                        管理画面にログイン
+                    </button>
+                </a>
+                <p class="p-note mb20">
+                    管理画面へのアクセスは代表者のみ
+                </p>
             </div>
 
-            <a href="<?= $this->Url->build(["controller"=>"Users","action"=>"add"]); ?>">
-                <button class="pure-button" type="button">
-                    ユーザー登録
-                </button>
-            </a>
+            </div>
         <?= $this->Form->end() ?>
     </div>
 
