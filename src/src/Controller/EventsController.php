0ccd013e037b7a8d0aab2498768b32113e2f82bb
lagrimas <lagrimas@everylive.jp>
2023-09-18 12:10:35 +0900

    fix

--- src/src/Controller/EventsController.php
+++ src/src/Controller/EventsController.php
@@ -277,22 +277,6 @@ class EventsController extends AppController
     }
 
     /**
-     * View method
-     *
-     * @param string|null $id Event id.
-     * @return \Cake\Http\Response|null|void Renders view
-     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
-     */
-    public function view($id = null)
-    {
-        $event = $this->Events->get($id, [
-            'contain' => ['Users', 'Locations', 'EventResponses'],
-        ]);
-
-        $this->set(compact('event'));
-    }
-
-    /**
      * Add method
      *
      * @return \Cake\Http\Response|null|void Redirects on successful add, renders view otherwise.
