a85b5097fa2c64564b307f13f3fc1c08a0fcce0d
lagrimas <lagrimas@everylive.jp>
2023-09-30 15:52:59 +0900

    fix

--- src/src/Controller/Admin/AdministratorsController.php
+++ src/src/Controller/Admin/AdministratorsController.php
@@ -261,21 +261,6 @@ class AdministratorsController extends AppController
                 $cnt = $all_count_data[$idx]['cnt'];
             }
 
-            // if(
-            //     isset($participants_count_data[$pcd_idx_diff])
-            //     && $all_count_data[$idx]['uid'] == $participants_count_data[$pcd_idx_diff]['uid']
-            // ){ //参照している配列のuidが一致している場合all_count(全イベント数) - participants_count(全部反応数)を算出する
-            //     $cnt = $all_count_data[$idx]['cnt'] - $participants_count_data[$$pcd_idx_diff]['cnt'];
-            //     // debug($all_count_data[$idx]['uid']);
-            //     // debug($participants_count_data[$idx - $pcd_idx_diff]['uid']);
-            //     // debug($all_count_data[$idx]['cnt']);
-            //     // debug($participants_count_data[$idx - $pcd_idx_diff]['cnt']);
-            //     $pcd_idx_diff = $pcd_idx_diff + 1;
-            // } else { //一度も反応してない人の場合全イベント数を未反応数とする
-            //     $cnt = $all_count_data[$idx]['cnt'];
-            // }
-
-
             $participants_count_list[] = [
                 'id' => $all_count_data[$idx]['uid'],
                 'display_name' => $all_count_data[$idx]['display_name'],
