93a95d3b762c0c6c77b2f58b9cfdc3b5a97a7db5
lagrimas <lagrimas@everylive.jp>
2023-10-04 10:47:06 +0900

    fix

---
+++ src/config/Migrations/20231003123503_RejectedToken.php
@@ -0,0 +1,38 @@
+<?php
+declare(strict_types=1);
+
+use Migrations\AbstractMigration;
+
+class RejectedToken extends AbstractMigration
+{
+    /**
+     * Up Method.
+     *
+     * More information on this method is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-up-method
+     * @return void
+     */
+    public function up(): void
+    {
+        $this->table('rejected_tokens')
+            ->addColumn('line_user_id', 'biginteger', [
+                'default' => null,
+                'limit' => null,
+                'null' => false,
+                'signed' => false,
+            ])
+            ->create();
+    }
+
+    /**
+     * Down Method.
+     *
+     * More information on this method is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-down-method
+     * @return void
+     */
+    public function down(): void
+    {
+        $this->table('rejected_tokens')->drop()->save();
+    }
+}
