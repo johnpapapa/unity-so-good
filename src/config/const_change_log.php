f142ad1d90abf4c07f904169f2605f99c5683069
lagrimas <lagrimas@everylive.jp>
2024-01-13 04:44:40 +0900

    add memo

--- src/config/const_change_log.php
+++ src/config/const_change_log.php
@@ -1,8 +1,9 @@
 <?php
+    //MEMO:更新履歴的なのを表示させたかった
     return [
       "change_log" => [ //["date"=>"12/06", "content"=>"sample"]
         ["date"=>"12/05", "content"=>"sample"]
-        //どんどん追加してく
+        //どんどん追加してくイメージ
       ]
     ];
 ?>
