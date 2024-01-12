d0a1d08e4b285a768cb1174c3f1eb340fd751472
lagrimas <lagrimas@everylive.jp>
2024-01-13 07:05:37 +0900

    秘密にしたい定数ファイルの削除

--- src/config/const_secret.php
+++
@@ -1,8 +0,0 @@
-<?php
-    return [
-        'param_linelogin_secret' => [
-            'client_id'     => '2000439541',
-            'client_secret' => 'b3b4212b5b7760b442883bb88b1f21f1',
-        ]
-    ];
-?>
